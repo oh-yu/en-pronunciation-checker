aa29e82a687b5d45582340a2be26007583cccf4d
hisasu00 <perusyanekolove0320gmail.com>
2020-04-20 22:51:42 -0700

    revise server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get("/",function(request,response){
-  response.sendFile(__dirname+"/index.html");
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
 
 });
 app.listen(process.env.PORT || 3000, function(){
