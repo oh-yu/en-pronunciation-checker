d38c0045ce47f3a36c05a83a3bc33c9d74266be0
hisasu00 <perusyanekolove0320gmail.com>
2020-04-20 22:16:22 -0700

    revise spelling

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var http = require('http');
-var fs = requier('fs');
+var fs = require('fs');
 
 var appServer = http.createServer()
 appServer.on('request', executeRequest);
 
 function executeRequest(req,res){
-  fs.readFile('./index.html','utf-8',callBack()=>{
+    fs.readFile('./index.html','utf-8',callBack()=>{
     res.writeHead(200,{'Content-Type': 'text/html'});
     res.write(data);
     res.end()
