5b5413723dcc3f26c1f31eb6bfc852ae5f7492aa
hisasu00 <perusyanekolove0320gmail.com>
2020-04-21 06:00:03 -0700

    introduce vue&vuetify

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get("/",function(req,res){
-  res.sendFile(__dirname+"/index.html");
-});
-app.listen(process.env.PORT || 3000, function(){
-  console.log("This is runnning on port 3000 correctly.");
-});
