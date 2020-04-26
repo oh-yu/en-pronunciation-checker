04d1c241163ec0a7e9eb47340d325da0a60770ca
hisasu00 <perusyanekolove0320gmail.com>
2020-04-26 05:49:30 -0700

    change style of info page

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <v-content>
-      <v-list>
-        <v-list-item-title>Source:</v-list-item-title>
-        <a href='https://github.com/hisasu00'>https://github.com/hisasu00</a>
-      </v-list>
-      <v-list>
-        <v-list-item-title>Email:</v-list-item-title>
-        <a href='mailto:a16.kk5e@g.chuo-u.ac.jp'>a16.kk5e@g.chuo-u.ac.jp</a>
-      </v-list>
-    </v-content>
+    <v-card class="mx-3">
+      <v-content>
+        <v-list>
+          <v-list-item-title>Source:</v-list-item-title>
+          <a href='https://github.com/hisasu00'>https://github.com/hisasu00</a>
+        </v-list>
+        <v-list>
+          <v-list-item-title>Email:</v-list-item-title>
+          <a href='mailto:a16.kk5e@g.chuo-u.ac.jp'>a16.kk5e@g.chuo-u.ac.jp</a>
+        </v-list>
+      </v-content>
+    </v-card>
   </div>
 </template>
 <script>
